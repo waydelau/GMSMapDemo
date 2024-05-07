6a1424125d59cd2dfb9942fdeb7e2d732f5487dd
luoke <luoke@fordeal.com>
2024-05-07 09:55:07 +0800

    Initial Commit

---
+++ GMSMapDemo/GMSMapDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GMSMapDemoApp.swift
+//  GMSMapDemo
+//
+//  Created by wxliu on 2024/5/7.
+//
+
+import SwiftUI
+
+@main
+struct GMSMapDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
